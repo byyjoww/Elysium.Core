3ea2c37882d70a42d121bab75d61f7b877472239
jonathan_aykroyd <olympusgate@gmail.com>
2021-05-19 00:41:33 +0100

    added scene related scripts to a scenes folder

---
+++ Runtime/Utils/Scenes/InitializableContainer.cs
@@ -0,0 +1,33 @@
+﻿using Elysium.Utils.Attributes;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Elysium.Utils
+{
+    public class InitializableContainer : MonoBehaviour
+    {
+        [SerializeField, RequireInterface(typeof(IInitializable))] private List<Object> initializables;
+        public List<IInitializable> Initializables => initializables.Cast<IInitializable>().ToList();
+
+        void Start()
+        {
+            DontDestroyOnLoad(this);
+#if UNITY_EDITOR
+
+            foreach (var i in Initializables)
+            {
+                i.Init();
+            }
+        }
+
+        void OnDestroy()
+        {
+            foreach (var i in Initializables)
+            {
+                i.End();
+            }
+#endif
+        }
+    }
+}
