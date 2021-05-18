ff075ee7487a4e3905929a98d868f87fa71848a8
byyjoww <64281249+byyjoww@users.noreply.github.com>
2021-05-19 00:33:23 +0100

    Update StringValueSO.cs

--- Runtime/Core/ValueSO/StringValueSO.cs
+++ Runtime/Core/ValueSO/StringValueSO.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace Elysium.Core
 {
-    [CreateAssetMenu(fileName = "StringValueSO", menuName = "Scriptable Objects/Scriptable Value/Primitive/String", order = 1)]
+    [CreateAssetMenu(fileName = "StringValueSO", menuName = "Scriptable Objects/Value/String", order = 1)]
     public class StringValueSO : GenericValueSO<string>
     {
     }
