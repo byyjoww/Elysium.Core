62ced43af0f68687baa7dd737fb5cc0356bc6b80
byyjoww <64281249+byyjoww@users.noreply.github.com>
2021-05-19 00:33:51 +0100

    Update IntValueSO.cs

--- Runtime/Core/ValueSO/IntValueSO.cs
+++ Runtime/Core/ValueSO/IntValueSO.cs
@@ -2,7 +2,7 @@
 
 namespace Elysium.Core
 {
-    [CreateAssetMenu(fileName = "IntValueSO", menuName = "Scriptable Objects/Scriptable Value/Primitive/Int", order = 1)]
+    [CreateAssetMenu(fileName = "IntValueSO", menuName = "Scriptable Objects/Value/Int", order = 1)]
     public class IntValueSO : GenericValueSO<int>
     {
     }
