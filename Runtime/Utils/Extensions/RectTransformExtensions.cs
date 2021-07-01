60c2410f9bfb00ce4bb072f466d65062c7c07d85
jonathan_aykroyd <olympusgate@gmail.com>
2021-07-01 09:38:00 +0100

    added rect transform extension for setting automatic anchors (stretch)

--- Runtime/Utils/Extensions/RectTransformExtensions.cs
+++ Runtime/Utils/Extensions/RectTransformExtensions.cs
@@ -6,24 +6,41 @@ namespace Elysium.Utils
 {
     public static class RectTransformExtensions
     {
-        public static float Left(this RectTransform _e)
+        public static float Left(this RectTransform _rect)
         {
-            return _e.position.x - _e.rect.width * _e.pivot.x;
+            return _rect.position.x - _rect.rect.width * _rect.pivot.x;
         }
 
-        public static float Right(this RectTransform _e)
+        public static float Right(this RectTransform _rect)
         {
-            return _e.position.x + _e.rect.width * (1f - _e.pivot.x);
+            return _rect.position.x + _rect.rect.width * (1f - _rect.pivot.x);
         }
 
-        public static float Top(this RectTransform _e)
+        public static float Top(this RectTransform _rect)
         {
-            return _e.position.y + _e.rect.height * (1f - _e.pivot.y);
+            return _rect.position.y + _rect.rect.height * (1f - _rect.pivot.y);
         }
 
-        public static float Bottom(this RectTransform _e)
+        public static float Bottom(this RectTransform _rect)
         {
-            return _e.position.y - _e.rect.height * _e.pivot.y;
+            return _rect.position.y - _rect.rect.height * _rect.pivot.y;
+        }
+
+        public static void Stretch(this RectTransform _rect)
+        {
+            if (_rect.parent == null) 
+            {
+                Debug.LogError($"Unable to stretch rect transform {_rect.name} as it has no parent");
+                return; 
+            }
+
+            RectTransform parent = _rect.parent as RectTransform;
+            _rect.anchoredPosition = parent.position;
+            _rect.anchorMin = new Vector2(0f, 0f);
+            _rect.anchorMax = new Vector2(1f, 1f);
+            _rect.pivot = new Vector2(0.5f, 0.5f);
+            _rect.offsetMin = Vector2.zero;
+            _rect.offsetMax = Vector2.zero;
         }
     }
 }
